2e4526fae31c81f2a5bf864b7891092a72651269
vishan <Vishanchathuranga2003@gmail.com>
2025-07-09 02:08:23 +0530

    commit

--- NoteNexaFront/src/service/noteService.ts
+++ NoteNexaFront/src/service/noteService.ts
@@ -1,18 +1,18 @@
 import type {Note, NoteDeleteForm} from "../models/Note.ts";
-import {apiClient_1} from "./apiClient.ts";
+import {apiClient_2} from "./apiClient.ts";
 
 export const addNote = async (note: Note) => {
-    const response = await apiClient_1.post("/notes/addNote", note);
+    const response = await apiClient_2.post("/notes/addNote", note);
     console.log("return response data",response.data);
     return response.data;
 }
 export const deleteNote = async (note: NoteDeleteForm) => {
-    const response = await apiClient_1.post("/notes/deleteNote", note);
+    const response = await apiClient_2.post("/notes/deleteNote", note);
     console.log("return response data",response.data);
     return response.data;
 }
 
 export const getAllNote = async () => {
-    const response = await apiClient_1.post("/notes/getAll");
+    const response = await apiClient_2.post("/notes/getAll");
     return response.data;
 }
